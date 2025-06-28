69c37bc33a00cd637f3e65f124b2c009e22c1454
Malthael972 <frankvaliquette123@gmail.com>
2025-06-28 15:45:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,18 +8,15 @@ Player.Update.hook((original, self, i) => {
     const bodySlot = self.armor[11];  // Vanity body
     const legSlot = self.armor[12];   // Vanity legs
 
-    // Avoid errors if one of the vanity slots is empty
-    const safe = (item, stat) => item && item[stat] ? item[stat] : 0;
+    const safe = (item) => item && item.type !== 0;
 
-    self.statDefense += safe(headSlot, "defense") + safe(bodySlot, "defense") + safe(legSlot, "defense");
-    self.meleeSpeed  += safe(headSlot, "meleeSpeed") + safe(bodySlot, "meleeSpeed") + safe(legSlot, "meleeSpeed");
-    self.moveSpeed   += safe(headSlot, "moveSpeed") + safe(bodySlot, "moveSpeed") + safe(legSlot, "moveSpeed");
-    self.maxRunSpeed += safe(headSlot, "maxRunSpeed") + safe(bodySlot, "maxRunSpeed") + safe(legSlot, "maxRunSpeed");
-    self.magicDamage += safe(headSlot, "magicDamage") + safe(bodySlot, "magicDamage") + safe(legSlot, "magicDamage");
-    self.meleeDamage += safe(headSlot, "meleeDamage") + safe(bodySlot, "meleeDamage") + safe(legSlot, "meleeDamage");
-    self.rangedDamage += safe(headSlot, "rangedDamage") + safe(bodySlot, "rangedDamage") + safe(legSlot, "rangedDamage");
-    self.minionDamage += safe(headSlot, "minionDamage") + safe(bodySlot, "minionDamage") + safe(legSlot, "minionDamage");
-    self.magicCrit   += safe(headSlot, "magicCrit") + safe(bodySlot, "magicCrit") + safe(legSlot, "magicCrit");
-    self.meleeCrit   += safe(headSlot, "meleeCrit") + safe(bodySlot, "meleeCrit") + safe(legSlot, "meleeCrit");
-    self.rangedCrit  += safe(headSlot, "rangedCrit") + safe(bodySlot, "rangedCrit") + safe(legSlot, "rangedCrit");
+    // Apply defense manually from vanity slots
+    if (safe(headSlot)) self.statDefense += headSlot.defense;
+    if (safe(bodySlot)) self.statDefense += bodySlot.defense;
+    if (safe(legSlot))  self.statDefense += legSlot.defense;
+
+    // Manually apply armor effects (damage, speed, crit, etc.)
+    if (safe(headSlot)) headSlot.UpdateEquip(self);
+    if (safe(bodySlot)) bodySlot.UpdateEquip(self);
+    if (safe(legSlot))  legSlot.UpdateEquip(self);
 });
