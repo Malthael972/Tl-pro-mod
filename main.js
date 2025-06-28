c390cc6ee34c4dcbf53acb7810fbc9f1577d7452
Malthael972 <frankvaliquette123@gmail.com>
2025-06-28 14:13:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const Player = new NativeClass("Terraria", "Player");
 const Main = new NativeClass("Terraria", "Main");
-
+const Item = new NativeClass("Terraria", "Item");
 
 Player.Update.hook((original, self, i) => {
     original(self, i);
-    const headSlot = self.armor[10];
-    const bodySlot = self.armor[11];
-    const legSlot = self.armor[12];
 
-    self.statDefense += (headSlot.defense + bodySlot.defense + legSlot.defense); 
+    const head = self.armor[10];  // Vanity head
+    const body = self.armor[11];  // Vanity body
+    const legs = self.armor[12];  // Vanity legs
+
+    // Add defense from vanity slots
+    self.statDefense += (head.defense + body.defense + legs.defense);
+
+    // Create Item instances from vanity armor
+    const headItem = Item.create(head.type);
+    const bodyItem = Item.create(body.type);
+    const legItem = Item.create(legs.type);
+
+    // Apply individual bonuses from each piece
+    headItem.updateEquip(self);
+    bodyItem.updateEquip(self);
+    legItem.updateEquip(self);
+
+    // Apply armor set bonus if valid set
+    if (bodyItem.isArmorSet(headItem, legItem)) {
+        bodyItem.updateArmorSet(self);
+    }
 });
