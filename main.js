cf0200e74f558ba29509b3226b9a182fc1c4969d
Malthael972 <frankvaliquette123@gmail.com>
2025-06-28 16:04:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,24 @@ Player.Update.hook((original, self, i) => {
     for (const item of vanitySlots) {
         if (!item || item.type === 0) continue;
 
-        // Apply defense manually
+        // Add defense
         self.statDefense += item.defense || 0;
 
-        // Try to apply the armor's stat bonuses
-        if (typeof item.UpdateEquip === "function") {
-            item.UpdateEquip(self);
-        }
+        // Manually apply key stats
+        if (item.meleeSpeed) self.meleeSpeed += item.meleeSpeed;
+        if (item.moveSpeed) self.moveSpeed += item.moveSpeed;
+        if (item.maxRunSpeed) self.maxRunSpeed += item.maxRunSpeed;
+
+        if (item.meleeDamage) self.meleeDamage += item.meleeDamage;
+        if (item.rangedDamage) self.rangedDamage += item.rangedDamage;
+        if (item.magicDamage) self.magicDamage += item.magicDamage;
+        if (item.minionDamage) self.minionDamage += item.minionDamage;
+
+        if (item.meleeCrit) self.meleeCrit += item.meleeCrit;
+        if (item.rangedCrit) self.rangedCrit += item.rangedCrit;
+        if (item.magicCrit) self.magicCrit += item.magicCrit;
+
+        if (item.statManaMax2) self.statManaMax2 += item.statManaMax2;
+        if (item.statLifeMax2) self.statLifeMax2 += item.statLifeMax2;
     }
 });
