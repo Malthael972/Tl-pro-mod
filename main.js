f0e3c51152ded9a381a70573fe271eec30b76400
Malthael972 <frankvaliquette123@gmail.com>
2025-06-28 16:59:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const Player = new NativeClass("Terraria", "Player");
+const Main = new NativeClass("Terraria", "Main");
+
+
+Player.Update.hook((original, self, i) => {
+    original(self, i);
+    const headSlot = self.armor[10];
+    const bodySlot = self.armor[11];
+    const legSlot = self.armor[12];
+
+    self.statDefense += (headSlot.defense + bodySlot.defense + legSlot.defense); 
+});
