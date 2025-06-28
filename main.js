93796febe25aae60c8e1483cb6fcfe404f964cb5
Malthael972 <frankvaliquette123@gmail.com>
2025-06-28 15:55:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 const Player = new NativeClass("Terraria", "Player");
-const Main = new NativeClass("Terraria", "Main");
 
 Player.Update.hook((original, self, i) => {
     original(self, i);
 
-    const headSlot = self.armor[10];  // Vanity head
-    const bodySlot = self.armor[11];  // Vanity body
-    const legSlot = self.armor[12];   // Vanity legs
+    const vanitySlots = [self.armor[10], self.armor[11], self.armor[12]];
 
-    const safe = (item) => item && item.type !== 0;
+    for (const item of vanitySlots) {
+        if (!item || item.type === 0) continue;
 
-    // Add defense from vanity
-    if (safe(headSlot)) self.statDefense += headSlot.defense;
-    if (safe(bodySlot)) self.statDefense += bodySlot.defense;
-    if (safe(legSlot))  self.statDefense += legSlot.defense;
+        // Apply defense manually
+        self.statDefense += item.defense || 0;
 
-    // Call UpdateEquip to apply other stats like melee speed, damage, crit, etc.
-    if (safe(headSlot) && typeof headSlot.UpdateEquip === "function") headSlot.UpdateEquip(self);
-    if (safe(bodySlot) && typeof bodySlot.UpdateEquip === "function") bodySlot.UpdateEquip(self);
-    if (safe(legSlot) && typeof legSlot.UpdateEquip === "function")  legSlot.UpdateEquip(self);
+        // Try to apply the armor's stat bonuses
+        if (typeof item.UpdateEquip === "function") {
+            item.UpdateEquip(self);
+        }
+    }
 });
